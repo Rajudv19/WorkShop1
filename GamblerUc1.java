eca000b5ecf677795654516a09bebba9cecb470a
Rajudv19 <rajudv1997@gmail.com>
2022-04-20 08:37:59 +0530

    Added Code for Gambler UC1

---
+++ GamblerUc1.java
@@ -0,0 +1,12 @@
+package com.BridgeLabs.WorkShop;
+
+public class GamblerUc1{
+	public static void main(String[] args){
+		
+		int stackPerDay = 100;
+		int betpGame = 1;
+		System.out.println("Stack per day is $" + stackPerDay);
+        System.out.println("Bet per Game is $"  + betpGame);
+        
+    }
+}
